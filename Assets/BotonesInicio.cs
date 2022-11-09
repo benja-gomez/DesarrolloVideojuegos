91fa5be4a4625facf404a8d5ed9cdd22b9f5df27
Ostap D <demcostap@gmail.com>
2022-11-08 21:29:29 -0300

    small fix

---
+++ Assets/BotonesInicio.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BotonesInicio : MonoBehaviour
+{
+    public Button yourButton;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener (TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        Cambiar();
+    }
+
+    public void Cambiar()
+    {
+        SceneManager.LoadScene("Level_GameScene_1");
+    }
+}
